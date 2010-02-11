4e7638f8c49e536fcd28a0a96d060954dd234bc9
Richard Crowley <r@rcrowley.org>
2010-02-11 18:29:16 +0000

    Cleaning up flags and dependencies.

--- php.sh
+++ php.sh
@@ -1,10 +1,15 @@
 ARCH=amd64
+PHP=http://us3.php.net/distributions
 
 for VERSION in 5.2.12 5.3.1; do
 	V=$(echo $VERSION | sed -r 's/^([0-9]+\.[0-9]+).*$/\1/')
 	debra init /tmp/php-$$
 
-	[ "5.3" != "$V" ] && MYSQL=", libmysqlclient16"
+	if [ "5.3" == "$V" ]; then
+		MYSQL=""
+	else
+		MYSQL=", libmysqlclient16"
+	fi
 	cat <<EOF >/tmp/php-$$/DEBIAN/control
 Package: opt-php-$VERSION
 Version: $VERSION-1
@@ -18,14 +23,13 @@ Description: Standalone PHP $VERSION.
 EOF
 
 	# Install PHP itself.
-	if [ "5.3" != "$V" ]; then
+	if [ "5.3" == "$V" ]; then
 		MYSQL="--with-mysql=mysqlnd --with-mysqli=mysqlnd"
 	else
 		MYSQL="--with-mysql --with-mysqli"
 	fi
-	debra sourceinstall /tmp/php-$$ \
-		http://us3.php.net/get/php-$VERSION.tar.bz2/from/this/mirror \
-		-f "--enable-cli --enable-cgi --enable-fastcgi --with-openssl --with-curl --with-zlib $MYSQL --enable-pdo --with-pdo-mysql --enable-pcntl --enable-sqlite-utf8 --with-libxml-dir=/usr --with-pear",
+	debra sourceinstall /tmp/php-$$ $PHP/php-$VERSION.tar.bz2 \
+		-f "--enable-cli --enable-cgi --enable-fastcgi --with-openssl --with-curl --with-zlib $MYSQL --enable-pdo --with-pdo-mysql --enable-pcntl --enable-sqlite-utf8 --with-libxml-dir=/usr --with-pear"
 
 	# Build and push a Debian package.
 	debra build /tmp/php-$$ opt-php-${VERSION}_${VERSION}-1_$ARCH.deb
