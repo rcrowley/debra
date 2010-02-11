914353d80c59837969c3cedfdcfab4b9746e15f1
Richard Crowley <r@rcrowley.org>
2010-02-11 18:29:32 +0000

    Added RubyGems and path corrections.

--- ruby.sh
+++ ruby.sh
@@ -1,4 +1,6 @@
 ARCH=amd64
+RUBY=ftp://ftp.ruby-lang.org/pub/ruby
+RUBYFORGE=http://rubyforge.org/frs/download.php
 
 for VERSION in 1.8.7-p249 1.9.1-p378; do
 	V=$(echo $VERSION | sed -r 's/^([0-9]+\.[0-9]+).*$/\1/')
@@ -17,9 +19,21 @@ Description: Standalone Ruby $VERSION.  This installation includes RubyGems.
 EOF
 
 	# Install Ruby itself.
-	debra sourceinstall /tmp/ruby-$$ \
-		ftp://ftp.ruby-lang.org/pub/ruby/$V/ruby-$VERSION.tar.gz \
-		-b "sh -c 'echo -e fcntl\nopenssl\nreadline\nzlib >ext/Setup; cat ext/Setup'"
+	debra sourceinstall /tmp/ruby-$$ $RUBY/$V/ruby-$VERSION.tar.gz \
+		-b "sh -c 'echo fcntl\\\nopenssl\\\nreadline\\\nzlib >ext/Setup'"
+
+	# Install RubyGems.
+	sourceinstall /tmp/python-$$/opt/ruby-$VERSION \
+		$RUBYFORGE/60718/rubygems-1.3.5.tgz \
+		-c "/tmp/ruby-$$/opt/ruby-$VERSION/bin/ruby setup.rb"
+
+	# Replace path to the temporary home of this Python interpreter with
+	# the path where it will be installed.
+	for PATHNAME in $(grep -rlI /tmp/ruby-$$ /tmp/ruby-$$); do
+		mv $PATHNAME $PATHNAME.sav
+		sed s/\\/tmp\\/ruby-$$// <$PATHNAME.sav >$PATHNAME
+		rm $PATHNAME.sav
+	done
 
 	# Build and push a Debian package.
 	debra build /tmp/ruby-$$ opt-ruby-${VERSION}_${VERSION}-1_$ARCH.deb
